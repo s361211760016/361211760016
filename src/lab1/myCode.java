636c8f37f434e17f451bad1d927de84be6e33b24
s361211760016 <airair1125@gmail.com>
2018-06-26 11:38:59 +0700

    Hello World

---
+++ src/lab1/myCode.java
@@ -0,0 +1,23 @@
+package lab1;
+
+public class myCode {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world.");
+        System.out.println("สุดารัตน์ กรดสีใหม่.");
+        System.out.println("361211760016.");
+        System.out.println("MIT211.");
+        System.out.println("ระบบสารสนเทศ.");
+
+
+
+
+
+
+    }
+
+
+
+}//class
